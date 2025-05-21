ee303f0f06c5b8669958e0f5f0ed428903b49852
iamguystephane <gstephane138@gmail.com>
2025-05-21 13:19:05 +0100

    added the created_at property

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
   password: { type: String, required: true },
+  created_at: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("User", userSchema);
